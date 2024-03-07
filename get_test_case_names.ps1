4da9ca716df04b39a8d1ba3da1740efe7e09502d
Stephen Charette <scharette@solutionstreet.com>
2024-03-07 09:52:37 -0500

    Add script to get all test case names

---
+++ get_test_case_names.ps1
@@ -0,0 +1,8 @@
+﻿# Specify the path to the directory containing the folders
+$directoryPath = "C:\Users\StephenCharette\source\repos\EWA.Coordination.Services.Tests\test_cases"
+
+# Get the list of folders in the directory
+$folderNames = Get-ChildItem -Path $directoryPath -Directory | Select-Object -ExpandProperty Name
+
+# Output the folder names
+$folderNames
