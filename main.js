c747b1fada9b4c0a2bcef56c41722bc7259aa5b4
Stephen Charette <scharette@solutionstreet.com>
2024-03-22 08:19:07 -0400

    Add use-existing-output option for main.js

--- main.js
+++ main.js
@@ -5,8 +5,18 @@ var runner = require('./lib/runner');
 var api = require('./lib/api');
 
 let test_case = process.argv[2];
-
-let environment = process.argv[3] || "develop";
+let environment;
+if(process.argv[3] != "develop" || process.argv[3] != "release" || process.argv[3] != "prod-test") {
+  environment = "develop";
+}
+else {
+  environment = process.argv[3];
+}
 let endpointUrl = api.get_endpoint_url(environment);
 
-runner.runTestCase(`./test_cases/${test_case}`, endpointUrl, environment);
+let options = {}
+// Retrieve the value after --exclude and remove all whitespace
+const useExistingOutputIndex = process.argv.indexOf('--use-existing-output');
+options["use-existing-output"] = useExistingOutputIndex == -1 ? false : true;
+
+runner.runTestCase(`./test_cases/${test_case}`, endpointUrl, environment, options);
